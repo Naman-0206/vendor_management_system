ca877e4c745a9ea959450031d4a6e8d1465f039c
Naman-0206 <edu.ngarg@gmail.com>
2024-05-06 23:58:52 +0530

    added signal for updating fulfilment rate of vendor

--- vendor_management_system/purchase_orders/signals.py
+++ vendor_management_system/purchase_orders/signals.py
@@ -1,4 +1,5 @@
 from django.dispatch import Signal, receiver
+from django.db.models.signals import post_save
 from .models import PurchaseOrder
 
 recalculate_avg_response_time_signal = Signal()
@@ -18,3 +19,16 @@ def recalculate_avg_response_time(sender, vendor, **kwargs):
 
         vendor.average_response_time = average_response_time
         vendor.save()
+
+
+@receiver(post_save, sender=PurchaseOrder)
+def recalculate_vendor_fullfilment_rate(sender, instance: PurchaseOrder, **kwargs):
+    if instance.status.lower() == 'completed':
+        vendor = instance.vendor
+        total_orders = PurchaseOrder.objects.filter(
+            vendor=instance.vendor).count()
+        if total_orders > 0:
+            completed_orders = PurchaseOrder.objects.filter(
+                vendor=instance.vendor, status__iexact='completed').count()
+            vendor.fulfilment_rate = completed_orders/total_orders*100
+            vendor.save()
