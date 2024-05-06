7593ce8b6b65851559deab9bdd8277d309cd2d9b
Naman-0206 <edu.ngarg@gmail.com>
2024-05-06 20:47:20 +0530

    added tests for performance of vendor

--- vendor_management_system/vendors/tests.py
+++ vendor_management_system/vendors/tests.py
@@ -2,11 +2,19 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from .models import Vendor
+from .serializers import VendorPerformanceSerializer
 
 
 class VendorProfileTests(TestCase):
     def setUp(self):
         self.client = APIClient()
+        self.vendor = Vendor.objects.create(
+            name='Test Vendor',
+            contact_details='test@example.com',
+            address='123 Test St',
+            vendor_code='TEST1234'
+        )
+        self.initial_vendor_count = Vendor.objects.count()
 
     def test_create_vendor(self):
         response = self.client.post('/api/vendors/', {
@@ -16,13 +24,13 @@ class VendorProfileTests(TestCase):
             'vendor_code': 'TEST123'
         })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Vendor.objects.count(), 1)
-        self.assertEqual(Vendor.objects.get().name, 'Test Vendor')
+        self.assertEqual(Vendor.objects.count(), self.initial_vendor_count+1)
+        self.assertEqual(Vendor.objects.get(pk='TEST123').name, 'Test Vendor')
 
     def test_list_vendors(self):
         response = self.client.get('/api/vendors/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 0)
+        self.assertEqual(len(response.data), self.initial_vendor_count)
 
         Vendor.objects.create(
             name='Test Vendor',
@@ -33,7 +41,7 @@ class VendorProfileTests(TestCase):
 
         response = self.client.get('/api/vendors/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(len(response.data), self.initial_vendor_count+1)
 
     def test_retrieve_vendor(self):
         vendor = Vendor.objects.create(
@@ -75,4 +83,12 @@ class VendorProfileTests(TestCase):
         )
         response = self.client.delete(f'/api/vendors/{vendor.vendor_code}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Vendor.objects.count(), 0)
+        self.assertEqual(Vendor.objects.count(), self.initial_vendor_count)
+
+    def test_get_vendor_performance(self):
+        response = self.client.get(
+            f'/api/vendors/{self.vendor.pk}/performance/')
+        serializer = VendorPerformanceSerializer(self.vendor)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
