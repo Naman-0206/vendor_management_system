48fecb3819dc244ba21bb3220f012ac13c2bb0ae
Naman-0206 <edu.ngarg@gmail.com>
2024-05-06 20:46:13 +0530

    added default value to status

--- vendor_management_system/purchase_orders/models.py
+++ vendor_management_system/purchase_orders/models.py
@@ -8,7 +8,7 @@ class PurchaseOrder(models.Model):
     order_date = models.DateField(auto_now_add=True)
     items = models.JSONField()
     quantity = models.PositiveIntegerField()
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, default="Order Placed")
 
     def __str__(self):
         return self.po_number
